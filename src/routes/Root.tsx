3f04585bf6b080d6f9b557836a93518b368a14ba
TheLocust3 <jake.kinsella@gmail.com>
2024-10-03 16:55:29 -0400

    Get search page looking alright

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
-import { SearchBar } from '../components/SearchBar'
+import { colors } from '../constants';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 65%;
 `;
 
 export const Title = styled.h1`
@@ -26,18 +26,86 @@ export const SearchArea = styled.div`
   height: 100%;
 `;
 
-export const SearchBarContainer = styled.div`
+export const SearchContainer = styled.div`
   width: 50%;
 `;
 
+export const Form = styled.form`
+  display:flex;
+  flex-direction:row;
+
+  outline: 1.5px solid ${colors.lightBlack};
+  border-radius: 5px;
+`;
+
+export const SearchBar = styled.input`
+  display: block;
+  box-sizing: border-box;
+
+  width: 100%;
+  height: 50px;
+
+  padding-left: 10px;
+  padding-right: 10px;
+
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
+
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const SearchButton = styled.button`
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
+
+  padding-left: 15px;
+  padding-right: 15px;
+
+  background: transparent;
+  border: none;
+  border-left: 1.5px solid ${colors.lightBlack};
+  cursor: pointer;
+
+  input:focus ~ & {
+    background: ${colors.whiteActive};
+
+    &:hover {
+      background: ${colors.lightGray};
+    }
+  }
+
+  &:hover {
+    background: ${colors.whiteActive};
+  }
+`;
+
 function Root() {
+  const [search, setSearch] = useState('');
+
+  function onSubmit(event: any) {
+    event.preventDefault();
+    console.log(`Search: ${search}`)
+  }
+
   return (
     <Container>
       <SearchArea>
-        <SearchBarContainer>
+        <SearchContainer>
           <Title>Search Somewhere</Title>
-          <SearchBar />
-        </SearchBarContainer>
+
+          <Form onSubmit={onSubmit}>
+            <SearchBar onChange={(event) => setSearch(event.target.value)} />
+            <SearchButton>Go</SearchButton>
+
+            <input type="submit" style={{ display: "none" }} />
+          </Form>
+        </SearchContainer>
       </SearchArea>
     </Container>
   );
